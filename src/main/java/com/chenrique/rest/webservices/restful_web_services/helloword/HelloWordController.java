592e7943c67e63238aea12ee65e1715a9343feee
Camilo Henrique <camilo.henri.ch@gmail.com>
2024-05-26 14:36:07 -0300

    feat: estudando RestApi

--- src/main/java/com/chenrique/rest/webservices/restful_web_services/helloword/HelloWordController.java
+++ src/main/java/com/chenrique/rest/webservices/restful_web_services/helloword/HelloWordController.java
@@ -1,6 +1,7 @@
 package com.chenrique.rest.webservices.restful_web_services.helloword;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
@@ -9,5 +10,15 @@ public class HelloWordController {
     @GetMapping("/hello-word")
     public String helloWord() {
         return "Hello Word!";
+    }
+
+    @GetMapping("/hello-word-bean")
+    public HelloWordBean helloWordBeaString() {
+        return new HelloWordBean("Hello Word!");
+    }
+
+    @GetMapping("/hello-word/path-variable/{name}")
+    public HelloWordBean helloWordPathVariable(@PathVariable String name) {
+        return new HelloWordBean(String.format("Hello Word, %s", name));
     }
 }
