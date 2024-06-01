a2e02fa1582a040e52ccdcda8cf706f42797d255
Camilo Henrique <camilo.henri.ch@gmail.com>
2024-06-01 10:33:22 -0300

    aula 21

--- src/main/java/com/chenrique/rest/webservices/restful_web_services/helloword/HelloWordController.java
+++ src/main/java/com/chenrique/rest/webservices/restful_web_services/helloword/HelloWordController.java
@@ -1,11 +1,20 @@
 package com.chenrique.rest.webservices.restful_web_services.helloword;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
 public class HelloWordController {
+    private MessageSource messageSource;
+
+    public HelloWordController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping("/hello-word")
     public String helloWord() {
@@ -20,5 +29,12 @@ public class HelloWordController {
     @GetMapping("/hello-word/path-variable/{name}")
     public HelloWordBean helloWordPathVariable(@PathVariable String name) {
         return new HelloWordBean(String.format("Hello Word, %s", name));
+    }
+
+    @GetMapping("/hello-word-inter")
+    public String helloWordInter() {
+
+        Locale locate = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("bom.dia.message", null, "Messagem padrão", locate);
     }
 }
